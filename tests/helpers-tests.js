73c1c8df35f1dd6773d0ae675eb0d3aac238f41f
Robert Schooley <robert.schooley@gmail.com>
2013-07-04 00:34:34 -0400

    purging tabs with fire

--- tests/helpers-tests.js
+++ tests/helpers-tests.js
@@ -1,118 +1,118 @@
 
 var should      = require('should'),
 
-	helpers		= require('../lib/helpers');
+    helpers     = require('../lib/helpers');
 
 describe('helpers', function () {
-	it('should return an object', function () {
-		should.exist(helpers);
-		helpers.should.be.a('object');
-	});
+    it('should return an object', function () {
+        should.exist(helpers);
+        helpers.should.be.a('object');
+    });
 
-	describe('#assets', function () {
-		var assetsFile = '';
+    describe('#assets', function () {
+        var assetsFile = '';
 
-		before(function () { 
-			assetsFile = process.cwd() + '/tests/assets.yml';
-		});
+        before(function () { 
+            assetsFile = process.cwd() + '/tests/assets.yml';
+        });
 
-		it('should have an "assets" function', function () {
-			should.exist(helpers.assets);
-		});
+        it('should have an "assets" function', function () {
+            should.exist(helpers.assets);
+        });
 
-		it('should return an object', function () {
-			var assets = helpers.assets(assetsFile);
+        it('should return an object', function () {
+            var assets = helpers.assets(assetsFile);
 
-			should.exist(assets);
-			assets.should.be.a('object');
-		});
-	});
+            should.exist(assets);
+            assets.should.be.a('object');
+        });
+    });
 
-	describe('#baseUrl', function () {
-		it('should have a "baseUrl" function', function () {
-			should.exist(helpers.baseUrl);
-		});
-	});
+    describe('#baseUrl', function () {
+        it('should have a "baseUrl" function', function () {
+            should.exist(helpers.baseUrl);
+        });
+    });
 
-	describe('#defaults', function () {
-		it('should have a "defaults" property', function () {
-			should.exist(helpers.defaults);
-			helpers.defaults.should.be.a('object');
-		});
+    describe('#defaults', function () {
+        it('should have a "defaults" property', function () {
+            should.exist(helpers.defaults);
+            helpers.defaults.should.be.a('object');
+        });
 
-		it('should default "assets file"', function () {
-			var assetsFile 		= helpers.defaults.assetsFile,
-				pathHasDefault 	= false;
+        it('should default "assets file"', function () {
+            var assetsFile      = helpers.defaults.assetsFile,
+                pathHasDefault  = false;
 
-			should.exist(assetsFile);
-		
-			// the path changes on how this is called because of process.cwd
-			// in the app it is typically where app/web/server.js resides
-			// in tests it is the test dir
-			// in upstart it is / (I think)
-			pathHasDefault = assetsFile.indexOf('/config/assets.yml') > -1;			
-			pathHasDefault.should.be.ok;
-		});
+            should.exist(assetsFile);
+        
+            // the path changes on how this is called because of process.cwd
+            // in the app it is typically where app/web/server.js resides
+            // in tests it is the test dir
+            // in upstart it is / (I think)
+            pathHasDefault = assetsFile.indexOf('/config/assets.yml') > -1;         
+            pathHasDefault.should.be.ok;
+        });
 
-		it('should default "debug" to false', function () {
-			var debug = helpers.defaults.debug;
+        it('should default "debug" to false', function () {
+            var debug = helpers.defaults.debug;
 
-			should.exist(debug);
-			debug.should.equal(false);
-		});
+            should.exist(debug);
+            debug.should.equal(false);
+        });
 
-		it('should default "express3" to true', function () {
-			var express3 = helpers.defaults.express3;
+        it('should default "express3" to true', function () {
+            var express3 = helpers.defaults.express3;
 
-			should.exist(express3);
-			express3.should.equal(true);
-		});
+            should.exist(express3);
+            express3.should.equal(true);
+        });
 
-		it('should default "minify" to false', function () {
-			var minify = helpers.defaults.minify;
+        it('should default "minify" to false', function () {
+            var minify = helpers.defaults.minify;
 
-			should.exist(minify);
-			minify.should.equal(false);
-		});
-	});
+            should.exist(minify);
+            minify.should.equal(false);
+        });
+    });
 
-	describe('#settings', function () {
-		var settings = {};
+    describe('#settings', function () {
+        var settings = {};
 
-		it('should have a "settings" function', function () {
-			should.exist(helpers.settings);
-		});
+        it('should have a "settings" function', function () {
+            should.exist(helpers.settings);
+        });
 
-		it('should not override values not passed in', function () {
-			settings = helpers.settings({ foo: true, bar: false });
+        it('should not override values not passed in', function () {
+            settings = helpers.settings({ foo: true, bar: false });
 
-			for (var key in helpers.defaults) {
-				settings[key].should.equal(helpers.defaults[key]); 
-			}
-		})
+            for (var key in helpers.defaults) {
+                settings[key].should.equal(helpers.defaults[key]); 
+            }
+        })
 
-		describe('should override specified value(s)', function () {
-			it('for "assetsFile"', function () {
-				var value = 'foo/bar/baz';
+        describe('should override specified value(s)', function () {
+            it('for "assetsFile"', function () {
+                var value = 'foo/bar/baz';
 
-				settings = helpers.settings({ assetsFile: value });
-				settings.assetsFile.should.equal(value);
-			});
+                settings = helpers.settings({ assetsFile: value });
+                settings.assetsFile.should.equal(value);
+            });
 
-			it('for "debug"', function () {
-				settings = helpers.settings({ debug: true });
-				settings.debug.should.equal(true);
-			});
+            it('for "debug"', function () {
+                settings = helpers.settings({ debug: true });
+                settings.debug.should.equal(true);
+            });
 
-			it('for "express3"', function () {
-				settings = helpers.settings({ express3: true });
-				settings.express3.should.equal(true);
-			});
+            it('for "express3"', function () {
+                settings = helpers.settings({ express3: true });
+                settings.express3.should.equal(true);
+            });
 
-			it('for "minify"', function () {
-				settings = helpers.settings({ minify: true });
-				settings.minify.should.equal(true);
-			});
-		});
-	});
+            it('for "minify"', function () {
+                settings = helpers.settings({ minify: true });
+                settings.minify.should.equal(true);
+            });
+        });
+    });
 });
--- tests/minimus-tests.js
+++ tests/minimus-tests.js
@@ -1,72 +1,72 @@
 
 var should      = require('should'),
 
-	minimus		= require('../lib/minimus');
+    minimus     = require('../lib/minimus');
 
 describe('minimus', function () {
-	describe('with settings', function () {
-		
-		describe('for express 2.x', function () {
-			var assets 		= {},
-				assetsFile 	= process.cwd() + '/tests/assets.yml';
+    describe('with settings', function () {
+        
+        describe('for express 2.x', function () {
+            var assets      = {},
+                assetsFile  = process.cwd() + '/tests/assets.yml';
 
-			before(function () {
-				assets = minimus({ assetsFile: assetsFile, express3: false });
-			});
+            before(function () {
+                assets = minimus({ assetsFile: assetsFile, express3: false });
+            });
 
-			it('should return an object', function () {
-				should.exist(assets);
-				assets.should.be.a('object');
-			});
+            it('should return an object', function () {
+                should.exist(assets);
+                assets.should.be.a('object');
+            });
 
-			it('should have a "javascripts" function', function () {
-				should.exist(assets.javascripts);
-				assets.javascripts.should.be.a('function');
-			});
+            it('should have a "javascripts" function', function () {
+                should.exist(assets.javascripts);
+                assets.javascripts.should.be.a('function');
+            });
 
-			it('should have a "stylesheets" function', function () {
-				should.exist(assets.stylesheets);
-				assets.stylesheets.should.be.a('function');
-			});
-		});
+            it('should have a "stylesheets" function', function () {
+                should.exist(assets.stylesheets);
+                assets.stylesheets.should.be.a('function');
+            });
+        });
 
-		describe('for express 3.x', function () {
-			var assets 		= {},
-				assetsFile 	= '',
-				req 		= {},
-				res 		= {};
+        describe('for express 3.x', function () {
+            var assets      = {},
+                assetsFile  = '',
+                req         = {},
+                res         = {};
 
-			before(function () {
-				assetsFile 	= process.cwd() + '/tests/assets.yml';
-				assets 		= minimus({ assetsFile: assetsFile, express3: true });
+            before(function () {
+                assetsFile  = process.cwd() + '/tests/assets.yml';
+                assets      = minimus({ assetsFile: assetsFile, express3: true });
 
-				res = {
-					locals: {}
-				}; 
-			});
+                res = {
+                    locals: {}
+                }; 
+            });
 
-			it('should return a function', function () {
-				should.exist(assets);
-				assets.should.be.a('function');
-			});
+            it('should return a function', function () {
+                should.exist(assets);
+                assets.should.be.a('function');
+            });
 
-			it('should call next()', function (done) {
-				assets(req, res, done);
-			});
+            it('should call next()', function (done) {
+                assets(req, res, done);
+            });
 
-			it('should set res.locals.javascripts to a function', function () {
-				assets(req, res, function () {
-					should.exist(res.locals.javascripts);
-					res.locals.javascripts.should.be.a('function');
-				});
-			});
+            it('should set res.locals.javascripts to a function', function () {
+                assets(req, res, function () {
+                    should.exist(res.locals.javascripts);
+                    res.locals.javascripts.should.be.a('function');
+                });
+            });
 
-			it('should set res.locals.stylesheets to a function', function () {
-				assets(req, res, function () {
-					should.exist(res.locals.stylesheets);
-					res.locals.stylesheets.should.be.a('function');
-				});
-			});
-		});
-	});
+            it('should set res.locals.stylesheets to a function', function () {
+                assets(req, res, function () {
+                    should.exist(res.locals.stylesheets);
+                    res.locals.stylesheets.should.be.a('function');
+                });
+            });
+        });
+    });
 });
